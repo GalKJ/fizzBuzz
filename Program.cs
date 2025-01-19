4307582cf08fb1aef06d9968a399bdf3e45364be
space doge <99536044+d0g3bl2@users.noreply.github.com>
2025-01-19 14:21:00 +0000

    Refactor FizzBuzz implementation to use FizzBuzzHelper class

--- Program.cs
+++ Program.cs
@@ -1,14 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// See https://aka.ms/new-console-template for more information
+
+using fizzBuzz;
+
 for (int i = 1; i <= 100; i++)
 {
-    Console.WriteLine(GetFizzBuzz(i));
-}
-
-string GetFizzBuzz(int number)
-{
-    if (number % 15 == 0) return "FizzBuzz";
-    if (number % 3 == 0) return "Fizz";
-    if (number % 5 == 0) return "Buzz";
-    return number.ToString();
+    Console.WriteLine(FizzBuzzHelper.GetFizzBuzz(i));
 }
