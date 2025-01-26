9a1717983692facf95c4fd8898d1cac2273745e4
space doge <99536044+d0g3bl2@users.noreply.github.com>
2025-01-26 18:22:01 +0000

    Add FizzBuzzHelper class with GetFizzBuzz method

---
+++ FizzBuzz/FizzBuzzHelper.cs
@@ -0,0 +1,13 @@
+namespace fizzBuzz
+{
+    public static class FizzBuzzHelper
+    {
+        public static string GetFizzBuzz(int number)
+        {
+            if (number % 15 == 0) return "FizzBuzz";
+            if (number % 3 == 0) return "Fizz";
+            if (number % 5 == 0) return "Buzz";
+            return number.ToString();
+        }
+    }
+}
